a15e83f45c1019aee54425db69db7e577260054f
Sara Heins <saraeheins@gmail.com>
2015-05-13 20:29:59 -0500

    Fix links, mobile nav, author attribution

--- wp-content/themes/ashleytebbe/js/mobile-nav.js
+++ wp-content/themes/ashleytebbe/js/mobile-nav.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     var screenWidth = $(document).width();
 
         $('#mobile-nav, #site-navigation ul li a').click(function(){
-            $('#site-navigation ul').slideToggle('slow');
+            $('#site-navigation ul').toggleClass('open');
         });
 
 });
